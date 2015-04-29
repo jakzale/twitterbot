1314df02e8fcfb0108a8cdbc912497721cfc27a9
Jakub Z <jakzale@gmail.com>
2015-04-29 21:56:13 +0100

    Cleaned it up

--- src/Simple.hs
+++ src/Simple.hs
@@ -1,21 +1,16 @@
 {-# LANGUAGE OverloadedStrings, DeriveGeneric #-}
-
 module Simple where
 
--- Credentials for OAuth
-
 import Data.Aeson
-import Data.Aeson.Types
 import Control.Monad
 import Web.Authenticate.OAuth
-import GHC.Generics
 
 -- loading credentials from JSON
 import qualified Data.ByteString.Lazy as L
 import qualified Data.ByteString.Char8 as C
 
 instance FromJSON C.ByteString where
-  parseJSON v =  liftM C.pack (parseJSON v :: Parser String)
+  parseJSON =  liftM C.pack . parseJSON
 
 instance FromJSON Credential where
   parseJSON (Object v) = newCredential <$> v .: "key" <*> v .: "secret"
