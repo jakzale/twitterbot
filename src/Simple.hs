e7c477675f5900b8cfb8100547ce34f1a069e3f6
Jakub Z <jakzale@gmail.com>
2015-04-29 21:54:08 +0100

    Simplified it

--- src/Simple.hs
+++ src/Simple.hs
@@ -11,24 +11,18 @@ import Web.Authenticate.OAuth
 import GHC.Generics
 
 -- loading credentials from JSON
-import qualified Data.ByteString as B
 import qualified Data.ByteString.Lazy as L
 import qualified Data.ByteString.Char8 as C
 
-data JSONCredentials =
-  JSONCredentials { key    :: !String
-                  , secret :: !String
-                  }
-  deriving (Show, Generic)
+instance FromJSON C.ByteString where
+  parseJSON v =  liftM C.pack (parseJSON v :: Parser String)
 
-instance FromJSON JSONCredentials
-
-g :: JSONCredentials -> Credential
-g json = newCredential (fmap C.pack key json) (fmap C.pack secret json)
+instance FromJSON Credential where
+  parseJSON (Object v) = newCredential <$> v .: "key" <*> v .: "secret"
 
 main :: IO ()
 main =
   do
     contents <- L.readFile "config.json"
-    putStrLn $ show $ liftM g $ (decode contents :: Maybe JSONCredentials)
+    putStrLn $ show $ (decode contents :: Maybe Credential)
 
