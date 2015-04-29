bbc3e5d18dcae7fd50b88337640a7b0b19283295
Jakub Z <jakzale@gmail.com>
2015-04-29 21:18:31 +0100

    WIP

--- src/Simple.hs
+++ src/Simple.hs
@@ -5,10 +5,22 @@ module Simple where
 -- Credentials for OAuth
 
 import Data.Aeson
+import Data.Aeson.Types
 import Control.Monad
 import Web.Authenticate.OAuth
 import GHC.Generics
 
+-- loading credentials from JSON
+import qualified Data.ByteString as B
+
+data JSONCredentials =
+  JSONCredentials { key    :: !String
+                  , secret :: !String
+                  }
+  deriving (Show, Generic)
+
+g :: JSONCredentials -> Credential
+g json = newCredential (key json) (secret json)
 
 main :: IO ()
 main = putStrLn "Hello, world!"
