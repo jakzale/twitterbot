ccaf5f1ed2a75c5b82a7936a621d3b6e0642acc4
Jakub Z <jakzale@gmail.com>
2015-04-30 12:03:27 +0100

    Start adding signing in to Twitter

--- src/Simple.hs
+++ src/Simple.hs
@@ -5,15 +5,35 @@ import Data.Aeson
 import Control.Monad
 import Web.Authenticate.OAuth
 
--- loading credentials from JSON
 import qualified Data.ByteString.Lazy as L
 import qualified Data.ByteString.Char8 as C
+
+import Data.Text (Text)
+import Data.Time.Clock (UTCTime)
+import GHC.Generics
+
+import Network.HTTP.Conduit
 
 instance FromJSON C.ByteString where
   parseJSON =  liftM C.pack . parseJSON
 
 instance FromJSON Credential where
   parseJSON (Object v) = newCredential <$> v .: "key" <*> v .: "secret"
+
+
+data Tweet =
+  Tweet { text       :: !Text
+        , created_at :: !UTCTime
+        } deriving (Show, Generic)
+
+instance FromJSON Tweet
+instance ToJSON Tweet
+
+-- timeline :: String -> IO (Either String [Tweet])
+-- timeline name = do
+--   req <- parseUrl $ "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=" ++ name
+--   res <- withManager $ \m -> do
+--     signedreq <- signedOAuth 
 
 main :: IO ()
 main =
